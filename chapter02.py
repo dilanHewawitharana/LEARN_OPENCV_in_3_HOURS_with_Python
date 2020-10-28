c8ac6db17a2477d6d578c9902d671d03ba257f75
Dilan Sarith Hewawitharana <42795493+dilanHewawitharana@users.noreply.github.com>
2020-10-28 15:52:52 +0900

    corrected spelling mistakes

--- chapter02.py
+++ chapter02.py
@@ -7,13 +7,13 @@ kernel = np.ones((5,5),np.uint8)
 imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY) # Convert to gray scale image
 imgBlur = cv2.GaussianBlur(imgGray,(7,7),0) # Blur operation
 imgCanny = cv2.Canny(img,150,200)
-imgDialation = cv2.dilate(imgCanny,kernel, iterations=1)
-imgEroded = cv2.erode(imgDialation,kernel,iterations=1)
+imgDilation = cv2.dilate(imgCanny,kernel, iterations=1)
+imgEroded = cv2.erode(imgDilation,kernel,iterations=1)
 
 cv2.imshow('Gray Image', imgGray)
 cv2.imshow('Blur Image', imgBlur)
 cv2.imshow('Canny Image', imgCanny)
-cv2.imshow('Dialation Image', imgDialation)
+cv2.imshow('Dilation Image', imgDilation)
 cv2.imshow('ErodedImage', imgEroded)
 
 cv2.waitKey(0)
